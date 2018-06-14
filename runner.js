7483c5043c12c9cc311ef93f4203d2015cb85181
pca006132 <john.lck40@gmail.com>
2018-06-14 13:10:48 +0800

    added error logging to runner

--- runner.js
+++ runner.js
@@ -1,7 +1,9 @@
 const child_process = require('child_process');
 
 const exec = (cmd)=>new Promise((resolve, reject)=>{
-    child_process.exec(cmd).on('exit', code=>resolve(code)).stdout.pipe(process.stdout);
+    const proc = child_process.exec(cmd).on('exit', code=>resolve(code));
+    proc.stdout.pipe(process.stdout);
+    proc.stderr.pipe(process.stdout);
 })
 
 async function run() {
