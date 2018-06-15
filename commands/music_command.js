fcd10f3cbbe46c1017a11cc978e348cf055a9edd
pca006132 <john.lck40@gmail.com>
2018-06-15 13:45:29 +0800

    fixed hasPermission bug

--- commands/music_command.js
+++ commands/music_command.js
@@ -7,19 +7,22 @@ module.exports = class MusicCommand extends Command {
         super(client, cmdinfo);
     }
     hasPermission(msg, ownerOverride = true) {
-        return super.hasPermission(msg, ownerOverride) && check(msg);
+        const result = super.hasPermission(msg, ownerOverride);
+        if (result === true) {
+            return check(msg);
+        } else {
+            return result
+        }
     }
 }
 
 function check(msg) {
     if (!msg.guild || !msg.member.voiceChannel) {
-        msg.say('You have to be in a voice channel to use this command');
-        return false;
+        return 'You have to be in a voice channel to use this command';
     }
     const channelName = msg.client.provider.get(msg.guild.id, 'music-channel', '.*');
     if (!(new RegExp(`^${channelName}$`)).exec(msg.channel.name)) {
-        msg.say(`You have to be in ${channelName} to use music commands`);
-        return false;
+        return `You have to be in ${channelName} to use music commands`;
     }
     return true;
 }
