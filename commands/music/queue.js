52ccfe1d5bb82f9480ead21fbe391554f2261f96
sylqiuyifeng <sylqiuyifeng@gmail.com>
2018-06-15 18:56:29 +0800

    :thinking:

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -21,12 +21,17 @@ module.exports = class Queue extends Command {
     async run(msg) {
         try {
             const player = getPlayer(msg.guild.id);
-            //create embed
+
             let embed = new RichEmbed();
             embed.setColor('GOLD');
             embed.setTitle('Playlist');
-            embed.setDescription(player.playlist.map((v, i) => i === player.index ? `**${v}**` : v).join('\n'));
-            embed.setFooter(`${player.index}/${player.playlist.length}`)
+
+            if (!player.index) {
+                embed.setDescription('The void,the null,the undefined and your loneliness')
+            } else {
+                embed.setDescription(player.playlist.map((v, i) => i === player.index ? `**${v}**` : v).join('\n'));
+                embed.setFooter(`${player.index}/${player.playlist.length}`)
+            }
             return msg.embed(embed);
         } catch (e) {
             msg.say(`Error: ${e}`);
