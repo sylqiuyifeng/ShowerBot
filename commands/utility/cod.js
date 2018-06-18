456c34fdfa786cac5242551aba0666226794151c
sylqiuyifeng <sylqiuyifeng@gmail.com>
2018-06-18 16:50:33 +0800

    it should work now

--- commands/utility/cod.js
+++ commands/utility/cod.js
@@ -21,10 +21,29 @@ module.exports = class COD extends Command {
     }
 
     hasPermission(msg, ownerOverride = false) {
-        return (msg.author.id === '254077247940460544'?true:'You are not SYL, you cannot wash dishes');
+        return (msg.author.id === '254077247940460544' ? true : 'You are not SYL, you cannot wash his dishes');
     }
 
     run(msg, args) {
-        return msg.say("You are SYL");
+        let embed = new RichEmbed();
+        embed.setTitle(':sweat_drops::fork_knife_plate:')
+        switch (this.state) {
+            case 0:
+                embed.setDescription('SYL starts playing - Call of Dishes:SoapWarfare');
+                this.startTime = new Date();
+                this.state = 1;
+                msg.embed(embed);
+                return msg.delete();
+
+            case 1:
+                embed.setDescription(`SYL finished playing - Call of Dishes:SoapWarfare\nUsed time:${(new Date() - this.startTime)/1000} seconds`);
+                this.state = 0;
+                msg.embed(embed);
+                return msg.delete();
+
+            default:
+                embed.setDescription('How do you find this? It shouldnt happen.');
+                return msg.embed(embed);
+        }
     }
 };
